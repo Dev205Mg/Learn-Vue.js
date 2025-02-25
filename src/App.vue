da50246c0c0b57b88c940313174b717de1095c77
Dev205 <dev205mg@gmail.com>
2025-02-25 13:43:59 +0300

    condition v-if, v-else, v-show

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <p>Compteur: {{ count }}</p>
+  <div v-show="count>=5">Bravo ! Vous avez cliquer 5 fois</div>
+  <div v-if="count <= 4">Non ! Pas encore cliquer 5 fois</div>
   <button @click="increment">Incrémenter</button>
   <button @click="count--">Décrementer</button>
 </template>
@@ -10,7 +12,6 @@
   const count = ref(0)
   const increment = (event) => {
     count.value++
-    console.log(event)
   }
 
 </script>
