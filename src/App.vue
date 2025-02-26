fd8c1567ad0de6a8376756f39c5b86f489110fab
Dev205 <dev205mg@gmail.com>
2025-02-26 15:28:23 +0300

    refair checked

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <form action="" @submit.prevent="addTodo">
+    <fieldset role="group">
+      <input 
+        type="text" 
+        placeholder="Ajouter un todo"
+        v-model="newTodo"
+        >
+      <button :disabled="newTodo.length <= 3">Ajouter</button>
+    </fieldset>
+  </form>
+
+  <div v-if="todos.length === 0">
+    Vous n'avez pas encore de tache :(
+  </div>
+  <div v-else>
+    <ul>
+      <li 
+        v-for="todo in todoSorted()"
+        :key="todo.date"
+        >
+        <label :class="{completed: todo.completed}">
+          <input 
+            type="checkbox" 
+            v-model="todo.completed"
+            :checked="todo.completed"
+            >
+          {{ todo.title }}
+        </label>
+      </li>
+
+    </ul>
+    <div>
+      <label>
+        <input 
+          type="checkbox"
+          v-model="hideTodos">
+        Masquer les taches qui sont deja fait 
+      </label>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+  const newTodo = ref('')
+  const hideTodos = ref(false)
+  const todos = ref([
+    { "title": "Acheter la propriété 'Rue de la Paix'", "completed": true, "date": 1 },
+    { "title": "Construire un hôtel sur 'Avenue Foch'", "completed": false, "date": 2 },
+  ])
+
+  const addTodo = () => {
+    todos.value.push({
+      title: newTodo.value,
+      completed: false,
+      date: Date.now()
+    })
+
+    newTodo.value = ''
+  }
+
+  const todoSorted = () => {
+    const todoSorted = todos.value.toSorted((a, b) => a.completed - b.completed)
+    if(hideTodos.value === true){
+      return todoSorted.filter(t => t.completed !== true)
+    }
+    return todoSorted
+  }
+  
+</script>
+
+<style>
+  .completed {
+    opacity: .5;
+    text-decoration: line-through;
+  }
+</style>
