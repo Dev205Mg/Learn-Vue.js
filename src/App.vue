921d7b7cf459f1f63b4e153afbb032cdc57c0f3b
Dev205 <dev205mg@gmail.com>
2025-02-27 12:39:45 +0300

    check

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <form action="" @submit.prevent="addTodo">
     <fieldset role="group">
-      <input 
-      type="text" 
-      placeholder="Ajouter votre todo"
-      v-model="newTodo">
+      <input type="text" placeholder="Ajouter votre todo" v-model="newTodo">
       <button :disabled="newTodo.length <= 3">Ajouter</button>
     </fieldset>
   </form>
@@ -13,70 +10,70 @@
   </div>
   <div v-else>
     <ul>
-    <li 
-      v-for="todo in sortedTodos"
-      :key="todo.date"
-      :class="{completed: todo.completed}"
-      >
-      <label>
-        <input type="checkbox" v-model="todo.completed">
-        {{ todo.title }}
-      </label>
-    </li>
-  </ul>
+      <li v-for="todo in sortedTodos" :key="todo.date" :class="{ completed: todo.completed }">
+        <Checkbox 
+          :label="todo.title"
+          v-model="todo.completed"
+          @check=" (p) => console.log('cocher', p)"
+          @uncheck="console.log('decocher')"/>
+      </li>
+    </ul>
 
-  <label>
-    <input type="checkbox" v-model="hideTodosCompleted">
-    Masquer les taches completées
-  </label>
+    <label>
+      <input type="checkbox" v-model="hideTodosCompleted">
+      Masquer les taches completées
+    </label>
 
-</div>
+  </div>
 
-<p v-if="remainingTodo > 0">
-  {{ remainingTodo }} tache{{ remainingTodo > 1 ? 's' : '' }} a faire
-</p>
+  <p v-if="remainingTodo > 0">
+    {{ remainingTodo }} tache{{ remainingTodo > 1 ? 's' : '' }} a faire
+  </p>
+
+  <Checkbox label="Bonjour"/>
 
 </template>
 
 <script setup>
-  import { computed, ref } from 'vue';
+import { computed, ref } from 'vue';
+import Checkbox from './Checkbox.vue';
 
-  const newTodo = ref('')
-  const hideTodosCompleted = ref(false)
-  const todos = ref([
-    { "title": "Acheter la propriété 'Rue de la Paix'", "completed": false, "date": 1 },
-    { "title": "Construire un hôtel sur 'Avenue Foch'", "completed": false, "date": 2 },
-    { "title": "Éviter la case prison", "completed": true, "date": 3 }
-  ])
+const newTodo = ref('')
+const hideTodosCompleted = ref(false)
+const todos = ref([
+  { "title": "Acheter la propriété 'Rue de la Paix'", "completed": false, "date": 1 },
+  { "title": "Construire un hôtel sur 'Avenue Foch'", "completed": false, "date": 2 },
+  { "title": "Éviter la case prison", "completed": true, "date": 3 }
+])
 
-  const addTodo = () => {
-    todos.value.push({
-      title: newTodo.value,
-      completed: false,
-      date: Date.now()
-    })
+const addTodo = () => {
+  todos.value.push({
+    title: newTodo.value,
+    completed: false,
+    date: Date.now()
+  })
 
-    newTodo.value = ''
+  newTodo.value = ''
+}
+
+const sortedTodos = computed(() => {
+  console.log('demo')
+  const sortedTodos = todos.value.toSorted((a, b) => a.completed > b.completed ? 1 : -1)
+  if (hideTodosCompleted.value === true) {
+    return sortedTodos.filter(t => t.completed === false)
   }
+  return sortedTodos
+})
 
-  const sortedTodos = computed(() =>{
-    console.log('demo')
-    const sortedTodos = todos.value.toSorted((a, b) => a.completed > b.completed ? 1 : -1)
-    if(hideTodosCompleted.value === true){
-      return sortedTodos.filter(t => t.completed === false)
-    }
-    return sortedTodos
-  })
-
-  const remainingTodo = computed(() => {
-    return todos.value.filter(t => t.completed === false).length
-  })
+const remainingTodo = computed(() => {
+  return todos.value.filter(t => t.completed === false).length
+})
 
 </script>
 
 <style>
-  .completed{
-    opacity: .5;
-    text-decoration: line-through;
-  }
+.completed {
+  opacity: .5;
+  text-decoration: line-through;
+}
 </style>
