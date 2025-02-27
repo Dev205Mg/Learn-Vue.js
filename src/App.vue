4558a679f5427a9e827b9836f6c30d343284d3c7
Dev205 <dev205mg@gmail.com>
2025-02-27 19:02:01 +0300

    composable

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
-
+  <input type="text" v-model="page.title">
+  Temps écoulé : {{ time }}
+  <button @click="reset">Reset</button>
 </template>
 
 <script setup>
+import { ref, watch, watchEffect } from 'vue';
+import { useTimer } from './composable/useTimer';
 
+const page = ref({
+  title: ''
+})
+const {time, reset} = useTimer()
+
+watchEffect(() => {
+  document.title = page.value.title
+})
 </script>
